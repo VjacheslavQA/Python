46040b9064f7f087fe37dcbcaaaede59701a06d0
podlypenskyi <viacheslav.podlypenskyi@nixsolutions.com>
2023-03-21 11:31:53 +0200

    Add task 5 (5_1_1_1; 5_1_1_2)

---
+++ Task5/5_1_1_2.py
@@ -0,0 +1,20 @@
+import requests
+
+
+url = "http://httpbin.org/post"
+headers = {"User-Agent": "Python Learning Requests"}
+form = {"comments": "Test post",
+        "custemail": "putler@kaput",
+        "custname": "Tester",
+        "custtel": "+38097654321",
+        "delivery": "12:15",
+        "size": "large",
+        "topping": ["bacon", "mushroom"]
+       }
+
+def post_request():
+    request = requests.post(url = url, headers = headers, data = form).json()
+    return request["form"], request["headers"]
+
+if __name__ == '__main__':
+    print(post_request())
